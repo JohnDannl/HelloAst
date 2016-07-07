76a1a1bf0b8b3bf864b707e35f29472043c34421
Jiang Dan[蒋旦] <jd5737@closeli.corp>
2016-07-07 11:45:30 +0800

    change lib to native code

---
+++ draggableViewPager-sample/src/main/java/com/arcsoft/closeli/draggableviewpager/callbacks/OnPageChangedListener.java
@@ -0,0 +1,8 @@
+package com.arcsoft.closeli.draggableviewpager.callbacks;
+
+import com.arcsoft.closeli.draggableviewpager.DraggableViewPager;
+
+public interface OnPageChangedListener {
+
+    public void onPageChanged(final DraggableViewPager draggableViewPager, final int newPageNumber);
+}
