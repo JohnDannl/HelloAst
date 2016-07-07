1512977313b1df1dce9c0c58d6019ac53eab6d3b
Jiang Dan[蒋旦] <jd5737@closeli.corp>
2016-07-07 13:49:13 +0800

    restore lib structure

--- draggableViewPager-sample/src/main/java/com/arcsoft/closeli/draggableviewpager/callbacks/OnPageChangedListener.java
+++
@@ -1,8 +0,0 @@
-package com.arcsoft.closeli.draggableviewpager.callbacks;
-
-import com.arcsoft.closeli.draggableviewpager.DraggableViewPager;
-
-public interface OnPageChangedListener {
-
-    public void onPageChanged(final DraggableViewPager draggableViewPager, final int newPageNumber);
-}
